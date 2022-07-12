992fd891d304aff8f0134252c73beeb08296cb16
Lemonsity <38927629+Lemonsity@users.noreply.github.com>
2022-07-11 22:42:32 -0400

    add detect the top-most visible tool section text

---
+++ src/content/ToolSectionText.ts
@@ -0,0 +1,32 @@
+export const TOOL_SECTION_CONTENT: {title: string, body: string}[] = [
+    {
+        title: 'Power up and down your server with just a few clicks',
+        body: 'Lodestone is the perfect tool for Minecraft server management on a local level. \
+        With just a few clicks, you can power up and down your server, as well as manage player permissions,\
+        mods, and plugins. Whether you\'re a beginner or a seasoned pro, \
+        Lodestone has the features you need to keep your server running smoothly.'
+    },
+    {
+        title: 'Manage player permissions and world settings',
+        body: 'Lodestone gives you the power to manage your players and worlds with ease. With our easy to use interface, \
+        you can quickly give or revoke player permissions, manage world settings, and more. Whether you\'re running a\
+        small server for friends or a large server for hundreds of players, Lodestone has the tools you need to keep things running smoothly.'
+    },
+    {
+        title: 'Generate detailed reports of server activity',
+        body: 'Lodestone gives server admins the ability to generate detailed reports of server activity, \
+        including player connections, disconnections, chat messages, and commands executed. \
+        These reports can be generated for specific time periods, and can be customized to \
+        include or exclude specific players, worlds, or server events.'
+    },
+    {
+        title: 'Get real-time alerts of server events',
+        body: 'Lodestone allows you to set up alerts for when certain events occur on your server, so you can stay up-to-date on what\'s happening even when you\'re away.'
+    },
+    {
+        title: 'And much, much more!',
+        body: 'The Lodestone team is committed to improving and adding new features to our client. \
+        We are constantly working on new ways to make our client more user-friendly and efficient. \
+        We want to make sure that our client is the best possible option for those who are looking for a Minecraft server hosting solution.'
+    }
+]
--- src/landingPage/ToolsSection.tsx
+++ src/landingPage/ToolsSection.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './ToolsSection.module.scss';
 import GhostBlock from 'common/GhostBlock';
+import { TOOL_SECTION_CONTENT } from '../content/ToolSectionText';
+import VisibilitySensor from 'react-visibility-sensor';
+
 
 const ToolsSection = (props: any) => {
+
+  const [sectionVisibility, setSectionVisibility] = useState<boolean[]>(TOOL_SECTION_CONTENT.map(()=>false))
+
   return (
     <section className={styles.container}>
       <h1>
@@ -11,51 +17,30 @@ const ToolsSection = (props: any) => {
       </h1>
 
       <ul className={styles.parameters}>
-        <li>
-          <h2>Power up and down your server with just a few clicks</h2>
-          <p>
-            Lodestone is the perfect tool for Minecraft server management on a local level. 
-            With just a few clicks, you can power up and down your server, as well as manage player permissions,
-            mods, and plugins. Whether you're a beginner or a seasoned pro, 
-            Lodestone has the features you need to keep your server running smoothly.
-          </p>
-        </li>
-        <li>
-          <h2>Manage player permissions and world settings</h2>
-          <p>
-          Lodestone gives you the power to manage your players and worlds with ease. With our easy to use interface, 
-          you can quickly give or revoke player permissions, manage world settings, and more. Whether you're running a
-          small server for friends or a large server for hundreds of players, Lodestone has the tools you need to keep things running smoothly.
-          </p>
-        </li>
-        <li>
-          <h2>Generate detailed reports of server activity</h2>
-          <p>
-          Lodestone gives server admins the ability to generate detailed reports of server activity, 
-          including player connections, disconnections, chat messages, and commands executed. 
-          These reports can be generated for specific time periods, and can be customized to 
-          include or exclude specific players, worlds, or server events.
-          </p>
-        </li>
-        <li>
-          <h2>Get real-time alerts of server events</h2>
-          <p>
-          Lodestone allows you to set up alerts for when certain events occur on your server, so you can stay up-to-date on what's happening even when you're away.
-          </p>
-        </li>
-        <li>
-          <h2>And much, much more!</h2>
-          <p>
-          The Lodestone team is committed to improving and adding new features to our client. 
-          We are constantly working on new ways to make our client more user-friendly and efficient. 
-          We want to make sure that our client is the best possible option for those who are looking for a Minecraft server hosting solution.
-          </p>
-        </li>
+        {TOOL_SECTION_CONTENT.map((content, index) => {
+          return (
+            <VisibilitySensor onChange={(isVisible: boolean) => {
+              const newSectionVisibility = [...sectionVisibility];
+              newSectionVisibility[index] = isVisible
+              setSectionVisibility(newSectionVisibility);
+              }}
+              >
+              <li>
+                  <h2>{content.title}</h2>
+                  <p>{content.body}</p>
+              </li>
+            </VisibilitySensor>
+          )
+        })}
       </ul>
 
       <div className={styles.stickyBoxBounds}>
         <div className={styles.stickyBox}>
-          <GhostBlock></GhostBlock>
+          <GhostBlock>
+            <p style={{color: 'white'}}>
+              {sectionVisibility.findIndex((truthness) => truthness)}
+            </p>
+          </GhostBlock>
         </div>
       </div>
     </section>
