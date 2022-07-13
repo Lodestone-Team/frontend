fe88e5ab4bf8af2532bed53c34b64f3e2a275280
Lemonsity <38927629+Lemonsity@users.noreply.github.com>
2022-07-12 21:39:06 -0400

    implement fade for tool section

--- src/landingPage/ToolsSection.tsx
+++ src/landingPage/ToolsSection.tsx
@@ -3,11 +3,13 @@ import styles from './ToolsSection.module.scss';
 import GhostBlock from 'common/GhostBlock';
 import { TOOL_SECTION_CONTENT } from '../content/ToolSectionText';
 import VisibilitySensor from 'react-visibility-sensor';
+import { Transition } from 'react-transition-group';
+import { placeholderTexts } from 'placeholders/ToolsSectionImage';
 
 
 const ToolsSection = (props: any) => {
 
-  const [sectionVisibility, setSectionVisibility] = useState<boolean[]>(TOOL_SECTION_CONTENT.map(()=>false))
+  const [sectionVisibility, setSectionVisibility] = useState<boolean[]>(TOOL_SECTION_CONTENT.map(()=>true))
 
   return (
     <section className={styles.container}>
@@ -23,6 +25,8 @@ const ToolsSection = (props: any) => {
               const newSectionVisibility = [...sectionVisibility];
               newSectionVisibility[index] = isVisible
               setSectionVisibility(newSectionVisibility);
+              // inFn[index](isVisible);
+              // console.log(in1)
               }}
               >
               <li>
@@ -37,14 +41,44 @@ const ToolsSection = (props: any) => {
       <div className={styles.stickyBoxBounds}>
         <div className={styles.stickyBox}>
           <GhostBlock>
-            <p style={{color: 'white'}}>
-              {sectionVisibility.findIndex((truthness) => truthness)}
-            </p>
+            {
+              placeholderTexts.map(((text, index) => (
+                  <Transition in={sectionVisibility[index]} timeout={500}>
+                    {state => (
+                      <p style={{
+                        position: 'absolute',
+                        ...defaultStyle,
+                        ...transitionStyles[state]
+                      }}>
+                        {text}
+                      </p>
+                    )}
+                  </Transition>
+                )
+              ))
+            }
+            
           </GhostBlock>
         </div>
       </div>
     </section>
   );
+};
+
+const defaultStyle = {
+  right: 0,
+  top: 0,
+  color: 'white',
+  transition: `opacity 500ms ease-in-out`,
+  opacity: 0,
+}
+
+const transitionStyles = {
+  entering: { opacity: 1 },
+  entered:  { opacity: 1 },
+  exiting:  { opacity: 0 },
+  exited:  { opacity: 0 },
+  unmounted: { opacity: 0 },
 };
 
 export default ToolsSection;
