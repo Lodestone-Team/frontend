3d2fdbea457f164ee73e29f68e0cd66f340ebb39
Lemonsity <38927629+Lemonsity@users.noreply.github.com>
2022-07-14 22:20:57 -0400

    make footer look half decent on mobile

--- src/landingPage/FooterSection.tsx
+++ src/landingPage/FooterSection.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './FooterSection.module.scss';
-import { ReactComponent as YourSVG } from '../assets/footerimg.svg';
+import image from '../assets/footerimg.svg';
 
 const FooterSection = (props: any) => {
   // make this do whatever a download button does.
@@ -8,30 +8,34 @@ const FooterSection = (props: any) => {
 
   return (
     <footer className={styles.footer}>
-      <h1>where will you go?</h1>
-      
-      <a href="/" className={styles.downloadButton}>
-        <button onClick={onClick}>download now!</button>
-      </a>
+      <div className={styles.leftColumn}>
+        <h1>where will you go?</h1>
 
-      <ul className={styles.content}>
-        {/* this is a link for uniformity, change on implementation */}
-        <li>
-          <a href="/">about us &#8594;</a>
-        </li>
-        <li>
-          <a href="/">work with us! &#8594;</a>
-        </li>
-        <li>
-          <a href="/">view our GitHub!&#8594;</a>
-        </li>
+        <a href="/" className={styles.downloadButton}>
+          <button onClick={onClick}>download now!</button>
+        </a>
 
-        <li className={styles.copyright}>
-          © COPYRIGHT LODESTONE 2022. ALL RIGHTS RESERVED.
-        </li>
-      </ul>
+        <ul className={styles.content}>
+          {/* this is a link for uniformity, change on implementation */}
+          <li>
+            <a href="/">about us &#8594;</a>
+          </li>
+          <li>
+            <a href="/">work with us! &#8594;</a>
+          </li>
+          <li>
+            <a href="/">view our GitHub!&#8594;</a>
+          </li>
 
-      <YourSVG className={styles.svg}></YourSVG>
+          <li className={styles.copyright}>
+            © COPYRIGHT LODESTONE 2022. ALL RIGHTS RESERVED.
+          </li>
+        </ul>
+      </div>
+
+      <img className={styles.svg} src={image} alt={"Walking Together"}/>
+      {/* <YourSVG className={styles.svg}></YourSVG> */}
+      {/* I want to use this, however it seems to behave weirdly when trying to define height */}
     </footer>
   );
 };
