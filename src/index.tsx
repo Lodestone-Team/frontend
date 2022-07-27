fc2002219bc24c8416c70989a0f76be88d9b53cd
Wenqi Huang <44907675+Ynng@users.noreply.github.com>
2022-07-27 14:41:32 -0400

    removed hard coded colors, used color variables

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import LandingPage from './landingPage/LandingPage';
 import reportWebVitals from './reportWebVitals';
 
