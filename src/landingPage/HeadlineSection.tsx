46210c8a5c763863a29f8ccdd220350a7d9547da
Mark <38927629+Lemonsity@users.noreply.github.com>
2022-07-29 00:41:57 -0400

    implement part of nav bar

--- src/landingPage/HeadlineSection.tsx
+++ src/landingPage/HeadlineSection.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
 import BarAnimation from './BarAnimation';
+import github from '../assets/GitHub-32px.png';
 
 import styles from './HeadlineSection.module.scss';
 
 const HeadlineSection = () => {
   return (
-    <section className={styles.container}>
-      <BarAnimation></BarAnimation>
-
+    <>
       <nav className={styles.header}>
-        <a href="/">point 1</a>
-        <a href="/">point 2</a>
-        <a href="/">point 3</a>
-        <a href="/">point 4</a>
+        <div className={styles.navLeft}>
+          <a href="/">about</a>
+          <a href="/">setup</a>
+          <a href="/">security</a>
+          <a href="/">control</a>
+          <a href="/">tools</a>
+        </div>
+        <div className={styles.navRight}>
+          <a href="/" >
+            <img className={styles.linkImage} src={github} alt="github logo" />
+          </a>
+        </div>
       </nav>
+      <section className={styles.container}>
+        <BarAnimation></BarAnimation>
 
-      <div className={styles.title}>
-        <h1>Lodestone</h1>
-        <h2>The most comprehensive Minecraft Server Management tool.</h2>
-      </div>
-    </section>
+        <div className={styles.title}>
+          <h1>Lodestone</h1>
+          <h2>The most comprehensive Minecraft Server Management tool.</h2>
+        </div>
+      </section>
+    </>
   );
 };
 
