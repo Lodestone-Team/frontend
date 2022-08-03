9c528d0c2e9a98c1253baf303f59e9d12de4efc5
Wenqi Huang <44907675+Ynng@users.noreply.github.com>
2022-08-03 18:59:47 -0400

    Removed extra classnames

--- src/landingPage/HeadlineSection.tsx
+++ src/landingPage/HeadlineSection.tsx
@@ -17,7 +17,7 @@ const HeadlineSection = () => {
 
       <div className={styles.headlineContainer}>
         <h1 className={styles.logo}>Lodestone</h1>
-        <h2 className={styles.h2}>The most comprehensive<br/>Minecraft Server Management tool.</h2>
+        <h2>The most comprehensive<br/>Minecraft Server Management tool.</h2>
       </div>
     </section>
   );
